43fa6293bb6454c30870d8a3cefcc4e470e7fb1e
Sirbu Ana <sirbuanca2@gmail.com>
2020-08-06 17:05:09 +0300

    Errors handling.

--- app/Http/Controllers/QuizAnswerController.php
+++ app/Http/Controllers/QuizAnswerController.php
@@ -10,18 +10,8 @@ use Symfony\Component\CssSelector\Node\SelectorNode;
 class QuizAnswerController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     */
-    public function index()
-    {
-
-    }
-
-    /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
      */
     public function create()
     {
@@ -36,64 +26,14 @@ class QuizAnswerController extends Controller
      */
     public function store(StoreQuizAnswer $request)
     {
-//        dd($request->all());
         $quiz_answer = new QuizAnswer();
         $tmp = $request->input("answer");
         $quiz_answer->$tmp = 1;
         $quiz_answer->save();
 
-        $quiz_answers = QuizAnswer::query()
+        return QuizAnswer::query()
             ->selectRaw("sum(good) as Good, sum(fair) as Fair, sum(neutral) as Neutral, sum(bad) as Bad")
             ->first()
             ->toArray();
-        $total_answers = array_sum($quiz_answers);
-
-        return view("store", compact("quiz_answers", "total_answers"));
-
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
--- resources/views/store.blade.php
+++
@@ -1,5 +0,0 @@
-<ol type="A">
-    @foreach($quiz_answers as $quiz_answer => $item)
-        <li>{{$quiz_answer." ". number_format($item / $total_answers * 100, 0) ."% - ".$item." results"}}</li>
-    @endforeach
-</ol>
